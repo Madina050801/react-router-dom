8833336abcdbc7ff96a1baeddc2d2cc57be2a5a0
abduraufova_ <92726131+Madina050801@users.noreply.github.com>
2022-01-20 23:49:55 +0500

    ssss

--- Filter/index.jsx
+++ Filter/index.jsx
@@ -4,15 +4,15 @@ import { useNavigate, useParams } from "react-router-dom";
 import { Wrapper } from "./style";
 // import { navbar } from "../../utils/navbar";
 
-const History = (props) => {
-  const params = useParams();
+const Filter = (props) => {
+  const params =useParams();
   console.log(props, params);
   // const navigate = useNavigate();
   return (
     <Wrapper>
-      <h1>Hi {params?.id}</h1>
+      <h1>Filter {params?.id}</h1>
     </Wrapper>
   );
 };
 
-export default History;
+export default Filter;
